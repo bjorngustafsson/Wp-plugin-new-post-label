2dd4ef92deadc2fa338f170307f443bc838ba1f2
Björn Gustafsson <bjogu419@gmail.com>
2015-05-28 10:19:39 +0200

    Made plugin oop and adding some comments

--- new-post-label.php
+++ new-post-label.php
@@ -9,38 +9,66 @@ Version: 0.1
 Author URI: http://bjorngustafsson.net/
 */
 
-
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-wp_register_script( 'my_plugin_script', plugins_url('/new-post-label.js', __FILE__), array('jquery'));
-wp_enqueue_script( 'my_plugin_script' );
+class BjornLabelNewPosts {
 
-//Styles
+	public static function init() {
+        $class = __CLASS__;
+        new $class;
+    }
 
-wp_register_style( 'namespace', plugins_url('styles.css', __FILE__) );
-wp_enqueue_style('namespace');  // wherever you want the css to load.
+	public function __construct() {
 
-function label_new_posts( $title ) {
+        //load textdomain in case want i18n
+		load_plugin_textdomain( 'label-new-posts-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
 
+        //Have to use array with $this since in a class
+        add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
+        //above is like in oop:
+        //$plugg = new BjornLabelNewPosts();
+		//$plugg->register_plugin_styles();
 
-	$time_diff_today_to_post = time() - get_the_time('U');
+        add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
 
-	//get timediff in days
- 	$days = date('d', $time_diff_today_to_post);
+        //maybe avoid hook in constructor if want to do unit test later
+        add_filter( 'the_title', array( $this, 'label_new_posts' ));
 
- 	//convert for example 05 days to 5 days
- 	$days_converted = intval($days);
+    }
 
- 	//Add label to recent posts, 2678400 = 30 days
-  if (in_the_loop() && is_home() && ((get_the_time('U') + 2678400) > time()) ) {
-		$title = "<span title='Lades till " . $days_converted . 
-		" dagar sedan.' class='new-post-label'>NYTT PROJEKT</span>" . $title;
-  	}
-  
-	return $title;
+    public function register_plugin_styles() {
+ 
+    wp_register_style( 'label-new-posts-plugin', plugins_url( 'new-post-label/css/styles.css' ) );
+    wp_enqueue_style( 'label-new-posts-plugin' );
 	}
 
-	add_filter( 'the_title', 'label_new_posts' );
+	public function register_plugin_scripts() {
+ 
+    wp_register_script( 'label-new-posts-plugin', plugins_url( 'new-post-label/js/new-post-label.js' ) );
+    wp_enqueue_script( 'label-new-posts-plugin' );
+}
 
+	public function label_new_posts( $title ) {
+
+		$time_diff_today_to_post = time() - get_the_time('U');
+
+        //get timediff in days
+        $days = date('d', $time_diff_today_to_post);
+
+        //convert for example 05 days to 5 days
+        $days_converted = intval($days);
+
+        //Add label to recent posts, 2678400 = 30 days
+		if (in_the_loop() && is_home() && ((get_the_time('U') + 2678400) > time()) ) {
+			$title = "<span title='Lades till " . $days_converted . 
+			" dagar sedan.' class='new-post-label'>NYTT PROJEKT</span>" . $title;
+			}
+
+		return $title;
+		}
+
+ }
+
+add_action( 'plugins_loaded', array( 'BjornLabelNewPosts', 'init' ) );
 
 ?>
